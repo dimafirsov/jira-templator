36f43a71da940c271869064fafe9341032f54ed2
Dima Firsov <dmitriy.firsov@gmail.com>
2018-01-16 11:26:26 +0200

    understood how to insert text

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,3 +1,6 @@
+var buttonSearch = document.getElementsByName('btnK')[0];
+console.log(buttonSearch);
+var element = document.getElementById('lst-ib');
 
-
-
+element.value = 'some other text';
+//buttonSearch.click();
