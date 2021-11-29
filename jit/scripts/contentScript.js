59ff2d9fbfbeddbe2c6152aec850548bd94aedb7
dimafirsov <dmitriy.firsov@gmail.com>
2021-11-29 21:47:25 +0200

    cleanup

--- jit/scripts/contentScript.js
+++ jit/scripts/contentScript.js
@@ -11,13 +11,11 @@ import * as toast from './toast';
         await setStorage({...DEFAULT_TEMPLATE});
         storage = (await getStorage())[STORAGE_NAME];
     }
-    console.log('>>> storage', storage);
 
     setTimeout(async () => {
         const mainPolling = setInterval(() => {
             newCreateButton = getGlobalTriggerElementFromStorage(storage);
             counter++;
-            console.log('>>>> link remained the same for ' + counter + ' seconds');
 
             if (newCreateButton !== oldCreateButton) {
                 console.info("[Jira Templator] - The 'Create' button's link changed. Refreshing links and subscriptions...");
